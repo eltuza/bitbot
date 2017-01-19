516a97b7a4cf5ca6429795d7f8dde58e0d09bd06
Martin Farias <eltuza@gmail.com>
2017-01-19 18:26:48 -0300

    preparing bot to echo  messages

--- index.js
+++ index.js
@@ -24,13 +24,38 @@ app.get('/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === '<bitbot_token></bitbot_token>') {
-        res.send(req.query['hub.challenge'])
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
     }
-    res.send('Error, wrong token')
+    res.sendStatus(200)
 })
 
 // Spin up the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+function sendTextMessage(sender, text) {
+  let messageData = { text:text }
+  request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: { access_token: BITBOT_PAGE_TOKEN },
+      method: 'POST',
+      json: {
+          recipient: { id: sender },
+          message: messageData,
+      }
+  }, function(error, response, body) {
+      if (error) {
+          console.log('Error sending messages: ', error)
+      } else if (response.body.error) {
+          console.log('Error: ', response.body.error)
+      }
+  })
+}
