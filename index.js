40f50b7d8bff8fc47d6be68eaf1309761aafa148
Martin Farias <eltuza@gmail.com>
2017-01-19 18:08:14 -0300

    seeing if we can import token safely

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
+const auth = require('./auth');
 const app = express()
 
 app.set('port', (process.env.PORT || 5000))
@@ -13,14 +14,16 @@ app.use(bodyParser.urlencoded({extended: false}))
 // Process application/json
 app.use(bodyParser.json())
 
+const token = auth.page_token;
+
 // Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
+    res.send('Hello world, I am a chat bot ' + token)
 })
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'bitbot_token') {
+    if (req.query['hub.verify_token'] === '<bitbot_token></bitbot_token>') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
