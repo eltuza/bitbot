87c2deb7cd009276df3cc414682cc241c662f2aa
Martin Farias <eltuza@gmail.com>
2017-01-20 20:53:32 -0300

    fix target url

--- converter.js
+++ converter.js
@@ -8,16 +8,16 @@ const BTC_SYMBOL = 'BTC';
 module.exports = {
   evaluate: function(message) {
     var tokens = message.split(' ');
-
+    let target;
     if (tokens.length === 1) {
       // Check if symbol or number.
       if (isNaN(tokens[0])) {
         // Ticker
         const ticker = tokens[0];
-        const target = `${BITCOINVERTER_BASE_URL}from=${BTC_SYMBOL}&to=${ticker}`;
+        target = `${BITCOINVERTER_BASE_URL}from=${BTC_SYMBOL}&to=${ticker}`;
       } else {
         const amount = tokens[0];
-        const target = `${BITCOINVERTER_BASE_URL}from=${BTC_SYMBOL}&to=${DEFAULT_FIAT_SYMBOL}&amount=${amount}`;
+        target = `${BITCOINVERTER_BASE_URL}from=${BTC_SYMBOL}&to=${DEFAULT_FIAT_SYMBOL}&amount=${amount}`;
       }
 
     } else if (tokens.length === 2) {
@@ -35,7 +35,7 @@ module.exports = {
       } else {
         return `Can't interpret ${tickers.join(' ')}`;
       }
-      const target = `${BITCOINVERTER_BASE_URL}from=${tickFrom}&to=${tickTo}&amount=${amount}`;
+      target = `${BITCOINVERTER_BASE_URL}from=${tickFrom}&to=${tickTo}&amount=${amount}`;
       console.log(target);
     } else {
       return `Can't interpret ${tickers.join(' ')}`;
